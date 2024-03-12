447999b4c2773ebb2cf3e4e85f781fe7786a4821
Gabriel Garcia-Miro Jeri <ggmj@Gabriels-MacBook-Pro-2.local>
2024-03-12 13:59:08 -0500

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,67 @@
+import React, { useState } from 'react';
 import './App.css'
 
-function App() {
- //Aquí tu código
-}
+// function App() {
+//  const [name, setName] = useState('Sofia');
+//  const [newName, setNewName] = useState('');
 
-export default App
+//  const handleClick = (newTeacherName) => {
+//     setName(newTeacherName);
+//  };
+
+//  const teachers = ['Data', 'Reyes', 'Yolanda'];
+
+//  return (
+//     <div>
+//         <h2>Teacher Name: {name}</h2>
+//         <ul>
+//             {teachers.map((teacher, index) => (
+//                 <li key={index} onClick={() => handleClick(teacher)}>
+//                     {teacher}
+//                 </li>
+//             ))}
+//         </ul>
+//     </div>
+//  );
+// };
+
+function App () {
+ const [name, setName] = useState('Sofia');
+ const [newName, setNewName] = useState('');
+ const [teachers, setTeachers] = useState(['Data', 'Reyes', 'Yolanda']);
+
+ const changeName = (event) => {
+    event.preventDefault();
+    if (newName.trim() !== '') {
+        if (!teachers.includes(newName)) {
+            setTeachers([...teachers, newName]);
+        }
+        setName(newName);
+        setNewName('');
+    }
+ };
+
+ return (
+    <div>
+        <h2>Teacher Name: {name}</h2>
+        <form onSubmit={changeName}>
+            <input 
+            type="text" 
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+            placeholder='Add a Name'
+            />
+            <button type='submit'>Submit</button>
+        </form>
+        <ul>
+            {teachers.map((teacher, index) => (
+                <li key={index} onClick={() => setName(teacher)}>
+                    {teacher}
+                </li>
+            ))}
+        </ul>
+    </div>
+ );
+};
+
+export default App;
